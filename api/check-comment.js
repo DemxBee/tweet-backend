342248592daff9843254edadc1c93e3b1c700839
DemxBee <138399338+DemxBee@users.noreply.github.com>
2025-06-25 12:04:04 +0100

    Add files via upload

---
+++ api/check-comment.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+export default async function handler(req, res) {
+  const { tweetId, username } = req.query;
+
+  const BEARER_TOKEN = decodeURIComponent('AAAAAAAAAAAAAAAAAAAAAI7W0wEAAAAA%2BulGuBjdJsBkpp4icVi9HMXMGdM%3Dtqg4dbQGeHGJ');
+  const query = `conversation_id:${tweetId} from:${username}`;
+  const url = `https://api.twitter.com/2/tweets/search/recent?query=${encodeURIComponent(query)}&tweet.fields=text,author_id,conversation_id`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: { Authorization: `Bearer ${BEARER_TOKEN}` }
+    });
+
+    const replies = response.data?.data || [];
+    res.status(200).json({ username, replies });
+  } catch (error) {
+    res.status(500).json({ error: error.message || 'Twitter API error' });
+  }
+}
